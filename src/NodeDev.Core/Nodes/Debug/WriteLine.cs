7e36176a6b9ce5fc534b5462cd7399c8a0cac0e2
snakex64 <pascx64@gmail.com>
2023-04-23 17:03:12 -0400

    Added basic NormalFlowNode that'll later handle 1 exec in - 1 exec out nodes

---
+++ src/NodeDev.Core/Nodes/Debug/WriteLine.cs
@@ -0,0 +1,19 @@
+﻿using NodeDev.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeDev.Core.Nodes.Debug
+{
+    public class WriteLine : NormalFlowNode
+    {
+        public WriteLine(Graph graph, string? id = null) : base(graph, id)
+        {
+            Name = "WriteLine";
+
+            Inputs.Add(new("Line", this, TypeFactory.CreateGenericType("T")));
+        }
+    }
+}
---
+++ src/NodeDev.Core/Nodes/NormalFlowNode.cs
@@ -0,0 +1,22 @@
+﻿using NodeDev.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeDev.Core.Nodes
+{
+    /// <summary>
+    /// Nodes that want to implement a normal flow should inherit from this class.
+    /// It'll automatically add a input exec and output exec.
+    /// </summary>
+    public abstract class NormalFlowNode : Node
+    {
+        protected NormalFlowNode(Graph graph, string? id = null) : base(graph, id)
+        {
+            Inputs.Add(new("exec", this, TypeFactory.ExecType));
+            Outputs.Add(new("exec", this, TypeFactory.ExecType));
+        }
+    }
+}
