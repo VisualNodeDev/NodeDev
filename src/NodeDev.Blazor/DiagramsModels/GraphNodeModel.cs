13d91bdec3755dc75c4593f244d58d54e8e4c094
snakex64 <pascx64@gmail.com>
2023-11-04 20:23:32 -0400

    Switched to new diagrams UI

---
+++ src/NodeDev.Blazor/DiagramsModels/GraphNodeModel.cs
@@ -0,0 +1,30 @@
+﻿using Blazor.Diagrams.Core.Models;
+using NodeDev.Blazor.NodeAttributes;
+using NodeDev.Core.Connections;
+using NodeDev.Core.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeDev.Blazor.DiagramsModels
+{
+    public class GraphNodeModel : NodeModel
+    {
+        internal readonly Node Node;
+
+        public GraphNodeModel(Node node) : base(new(node.GetOrAddDecoration<NodeDecorationPosition>(() => new(Vector2.Zero)).X, node.GetOrAddDecoration<NodeDecorationPosition>(() => new(Vector2.Zero)).Y))
+        {
+            Node = node;
+        }
+
+        public GraphPortModel GetPort(Connection connection) => Ports.OfType<GraphPortModel>().First( x=> x.Connection == connection);
+
+        internal void UpdateNodeBaseInfo(Node node)
+        {
+
+        }
+    }
+}
---
+++ src/NodeDev.Blazor/DiagramsModels/GraphPortModel.cs
@@ -0,0 +1,29 @@
+﻿using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Core.Models.Base;
+using NodeDev.Blazor.Components;
+using NodeDev.Core.Connections;
+
+namespace NodeDev.Blazor.DiagramsModels;
+
+public class GraphPortModel: PortModel
+{
+    internal readonly Connection Connection;
+
+    internal string PortColor => GraphCanvas.GetTypeShapeColor(Connection.Type, Connection.Parent.TypeFactory);
+
+    public GraphPortModel(GraphNodeModel parent, Connection connection, bool isInput) : base(parent, isInput ? PortAlignment.Left : PortAlignment.Right)
+    {
+        Connection = connection;
+    }
+
+    public override bool CanAttachTo(ILinkable other)
+    {
+        if(!base.CanAttachTo(other))
+            return false;
+
+        if (other is not GraphPortModel otherPort)
+            return false;
+
+        return Connection.Type.IsAssignableTo(otherPort.Connection.Type, out _);
+    }
+}
