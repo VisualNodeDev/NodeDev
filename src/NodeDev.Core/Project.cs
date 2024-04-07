a111134ed47e1ba7c7fd96bbef5b36d80c78d785
snakex64 <pascx64@gmail.com>
2024-04-06 20:28:01 -0400

    Added preprocessing to graphs before execution

--- src/NodeDev.Core/Project.cs
+++ src/NodeDev.Core/Project.cs
@@ -71,15 +71,31 @@ public class Project
 		NodeClassTypeCreator = new();
 		NodeClassTypeCreator.CreateProjectClassesAndAssembly(this);
 
+		// Before we start executing the project we need to preprocess every graph everywhere
+		foreach (var nodeClass in Classes)
+		{
+			foreach (var method in nodeClass.Methods)
+			{
+				method.Graph.PreprocessGraph();
+			}
+		}
+
+		// Find the main method
 		var program = Classes.Single(x => x.Name == "Program");
 
+		// Find the main method in the program class
 		var main = program.Methods.Single(x => x.Name == "Main");
+
+		// Create a new graph executor for the main method
 		GraphExecutor = new GraphExecutor(main.Graph, null);
 
 		try
 		{
+			// Execute the main method
 			var outputs = new object[main.ReturnType == TypeFactory.Get(typeof(void), null) ? 1 : 2]; // 1 for the exec, 2 for exec + the actual return value
 			GraphExecutor.Execute(null, inputs, outputs);
+
+			// Return the last output
 			return outputs[^1];
 		}
 		finally
