c4a68990997edb59d5c18df741d7b2ec59394d39
snakex64 <pascx64@gmail.com>
2024-04-13 21:38:47 -0400

    Optimize path generation to allow backward vertices

--- src/NodeDev.Blazor/DiagramsModels/SmoothPathGeneratorWithDirectVertices.cs
+++ src/NodeDev.Blazor/DiagramsModels/SmoothPathGeneratorWithDirectVertices.cs
@@ -25,7 +25,7 @@ public class SmoothPathGeneratorWithDirectVertices : PathGenerator
 		if (route.Length > 2)
 			return CurveThroughPoints(route, link);
 
-		route = GetRouteWithCurvePoints(link, route);
+		route = GetRouteWithCurvePoints(link, route[0], route[1], true, true);
 		double? sourceAngle = null;
 		double? targetAngle = null;
 
@@ -66,7 +66,7 @@ public class SmoothPathGeneratorWithDirectVertices : PathGenerator
 
 		for (var i = 0; i < route.Length - 1; i++)
 		{
-			var localRoute = GetRouteWithCurvePoints(link, route[i..(i + 2)]);
+			var localRoute = GetRouteWithCurvePoints(link, route[i], route[i + 1], i == 0, i + 2 == route.Length);
 
 			fullPath.AddCubicBezierCurve(localRoute[1].X, localRoute[1].Y, localRoute[2].X, localRoute[2].Y, localRoute[3].X, localRoute[3].Y);
 			paths[i] = new SvgPath().AddMoveTo(route[i].X, route[i].Y).AddCubicBezierCurve(localRoute[1].X, localRoute[1].Y, localRoute[2].X, localRoute[2].Y, localRoute[3].X, localRoute[3].Y);
@@ -76,33 +76,41 @@ public class SmoothPathGeneratorWithDirectVertices : PathGenerator
 		return new PathGeneratorResult(fullPath, paths, sourceAngle, route[0], targetAngle, route[^1]);
 	}
 
-	private Point[] GetRouteWithCurvePoints(BaseLinkModel link, Span<Point> route)
+	private Point[] GetRouteWithCurvePoints(BaseLinkModel link, Point p0, Point p1, bool isFirstInRoute, bool isLastInRoute)
 	{
-		var cX = (route[0].X + route[1].X) / 2;
-		var cY = (route[0].Y + route[1].Y) / 2;
-		var curvePointA = GetCurvePoint(route, link.Source, route[0].X, route[0].Y, cX, cY, first: true);
-		var curvePointB = GetCurvePoint(route, link.Target, route[1].X, route[1].Y, cX, cY, first: false);
-		return [route[0], curvePointA, curvePointB, route[1]];
+		var cX = (p0.X + p1.X) / 2;
+		var cY = (p0.Y + p1.Y) / 2;
+		var curvePointA = GetCurvePoint(link.Source, p0.X, p0.Y, cX, cY, first: true, isFirstInRoute, isLastInRoute);
+		var curvePointB = GetCurvePoint(link.Target, p1.X, p1.Y, cX, cY, first: false, isFirstInRoute, isLastInRoute);
+		return [p0, curvePointA, curvePointB, p1];
 	}
 
-	private Point GetCurvePoint(Span<Point> route, Anchor anchor, double pX, double pY, double cX, double cY, bool first)
+	private Point GetCurvePoint(Anchor anchor, double pX, double pY, double cX, double cY, bool first, bool isFirstInRoute, bool isLastInRoute)
 	{
 		if (anchor is PositionAnchor)
 			return new Point(cX, cY);
 
 		if (anchor is SinglePortAnchor spa)
 		{
-			return GetCurvePoint(pX, pY, cX, cY, spa.Port.Alignment);
+			PortAlignment portAlignment;
+			if((isFirstInRoute && first) || (isLastInRoute && !first))
+				portAlignment = spa.Port.Alignment;
+			else if(pX < cX)
+				portAlignment = PortAlignment.Right;
+			else
+				portAlignment = PortAlignment.Left;
+
+			return GetCurvePoint(pX, pY, cX, cY, portAlignment);
 		}
 		else if (anchor is ShapeIntersectionAnchor or DynamicAnchor or LinkAnchor)
 		{
-			if (Math.Abs(route[0].X - route[1].X) >= Math.Abs(route[0].Y - route[1].Y))
+			if (Math.Abs(pX - cX) >= Math.Abs(pY - cY))
 			{
-				return first ? new Point(cX, route[0].Y) : new Point(cX, route[1].Y);
+				return first ? new Point(cX, pY) : new Point(cX, cY);
 			}
 			else
 			{
-				return first ? new Point(route[0].X, cY) : new Point(route[1].X, cY);
+				return first ? new Point(pX, cY) : new Point(cX, cY);
 			}
 		}
 		else
