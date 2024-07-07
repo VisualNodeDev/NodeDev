a7f4d31e63740f691e81e4496cb6824728045b0c
snakex64 <pascx64@gmail.com>
2024-07-06 20:01:25 -0400

    Added WhileNode implementation

--- src/NodeDev.Core/Nodes/Flow/WhileNode.cs
+++ src/NodeDev.Core/Nodes/Flow/WhileNode.cs
@@ -35,7 +35,22 @@ public class WhileNode : FlowNode
 	public override bool DoesOutputPathAllowMerge(Connection execOutput) => execOutput == Outputs[1]; // Only the ExecOut path can merge. The loop path can never merge and always ends in a dead end.
 
 	internal override Expression BuildExpression(Dictionary<Connection, Graph.NodePathChunks>? subChunks, BuildExpressionInfo info)
-	{
-		throw new NotImplementedException();
-	}
+    {
+		ArgumentNullException.ThrowIfNull(subChunks);
+
+		var loopBody = Expression.Block(Graph.BuildExpression(subChunks[Outputs[0]], info)); // Build the loop body
+		var afterLoop = Expression.Block(Graph.BuildExpression(subChunks[Outputs[1]], info)); // Build the after loop body
+
+        var breakLabel = Expression.Label();
+        var loop = Expression.Loop(
+            Expression.IfThenElse(
+				info.LocalVariables[Inputs[1]], // while condition
+                loopBody, // does the assign for enumerator.Current, as well as the loop body
+                Expression.Break(breakLabel) // break the loop
+            ),
+            breakLabel
+        );
+
+		return Expression.Block(loop, afterLoop);
+    }
 }
