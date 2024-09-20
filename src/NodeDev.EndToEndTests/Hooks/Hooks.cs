e1d04ab08925a46f14dbe68d71b0985a2d5ca843
snakex64 <39806655+snakex64@users.noreply.github.com>
2024-09-20 00:07:50 -0400

    Setup CI to run e2e tests (#45)
    * Setup CI to run e2e tests

--- src/NodeDev.EndToEndTests/Hooks/Hooks.cs
+++ src/NodeDev.EndToEndTests/Hooks/Hooks.cs
@@ -9,20 +9,58 @@ public class Hooks
 {
     public IPage User { get; private set; } = null!; //-> We'll call this property in the tests
 
-    private static Process App;
+    private static Process App = null!;
+    private static StreamWriter StdOutput = null!;
+    private static StreamWriter StdError = null!;
 
     private const int Port = 5166;
 
     [BeforeFeature]
     public static async Task StartServer()
     {
-        App = Process.Start(new ProcessStartInfo()
+        StdOutput = new StreamWriter(File.Open("../../../../NodeDev.Blazor.Server/logs_std.txt", FileMode.Create));
+        StdError = new StreamWriter(File.Open("../../../../NodeDev.Blazor.Server/logs_err.txt", FileMode.Create));
+
+        // start the server using either a environment variable set by the CI, or a default path.
+        // The default path will work if you're running the tests from Visual Studio.
+        App = new Process();
+        App.StartInfo = new ProcessStartInfo()
         {
-            CreateNoWindow = false,
             FileName = "dotnet",
-            Arguments = "run --no-build",
-            WorkingDirectory = @"..\..\..\..\NodeDev.Blazor.Server",
-        })!;
+            Arguments = $"run --no-build -- --urls http://localhost:{Port}",
+            WorkingDirectory = "../../../../NodeDev.Blazor.Server",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+
+        App.OutputDataReceived += App_OutputDataReceived;
+        App.ErrorDataReceived += App_ErrorDataReceived;
+
+        App.Start();
+        App.BeginOutputReadLine();
+        App.BeginErrorReadLine();
+
+        await Task.Delay(1000);
+
+        if(App.HasExited)
+        {
+            StdOutput.Flush();
+            StdError.Flush();
+            throw new Exception("Failed to start the server: " + App.ExitCode);
+        }
+    }
+
+    private static void App_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data != null)
+            StdError.WriteLine(e.Data);
+    }
+
+    private static void App_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if(e.Data != null)
+            StdOutput.WriteLine(e.Data);
     }
 
     [BeforeScenario] // -> Notice how we're doing these steps before each scenario
@@ -33,7 +71,7 @@ public class Hooks
         //Initialise a browser - 'Chromium' can be changed to 'Firefox' or 'Webkit'
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false // -> Use this option to be able to see your test running
+            Headless = Environment.GetEnvironmentVariable("HEADLESS") == "true" // -> Use this option to be able to see your test running
         });
         //Setup a browser context
         var context1 = await browser.NewContextAsync();
@@ -50,11 +88,15 @@ public class Hooks
             }
             catch
             {
-                if (i == 5)
+                if (i == 60)
+                {
+                    StdOutput.Flush();
+                    StdError.Flush();
                     throw;
+                }
             }
 
-            await Task.Delay(100);
+            await Task.Delay(1000);
         }
     }
 
@@ -67,5 +109,12 @@ public class Hooks
         {
             await Task.Delay(100);
         }
+
+
+        StdOutput.Flush();
+        StdError.Flush();
+
+        StdError.Dispose();
+        StdError.Dispose();
     }
 }
