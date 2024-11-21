46b0dd56c4f6c39bbf07c8e090663f700b91a5b3
snakex64 <pascx64@gmail.com>
2024-11-20 19:49:34 -0500

    Added basic MAUI desktop app

---
+++ src/NodeDev.Blazor.MAUI/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using NodeDev.Blazor.Services;
+
+namespace NodeDev.Blazor.MAUI
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			var blazorWebView = builder.Services.AddMauiBlazorWebView();
+
+			blazorWebView.Services.AddNodeDev();
+
+#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
