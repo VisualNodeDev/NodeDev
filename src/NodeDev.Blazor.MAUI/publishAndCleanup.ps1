c035bc92003286db27aae1e19a2ef9e3ce390445
snakex64 <pascx64@gmail.com>
2024-11-22 18:27:16 -0500

    Added new release packages to pipelines

--- src/NodeDev.Blazor.MAUI/publishAndCleanup.ps1
+++ src/NodeDev.Blazor.MAUI/publishAndCleanup.ps1
@@ -1,5 +1,10 @@
+param (
+    [string]$architecture,
+	[bool]$selfContained
+ )
 
-dotnet publish -c Release --runtime win-x64
+
+dotnet publish -c Release --runtime $architecture --self-contained $selfContained
 
 # Remove all folders except wwwroot and en-us
 # Set array of folders to keep
@@ -7,7 +12,7 @@ dotnet publish -c Release --runtime win-x64
 $foldersToKeep = @("wwwroot", "en-us")
 
 # Get all folders in the publish folder
-$folders = Get-ChildItem -Path .\bin\Release\net9.0-windows10.0.19041.0\win-x64\publish -Directory
+$folders = Get-ChildItem -Path .\bin\Release\net9.0-windows10.0.19041.0\$architecture\publish -Directory
 
 # Delete folders that are not in the foldersToKeep array
 foreach ($folder in $folders) {
